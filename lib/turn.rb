83e96da35ab751594da4e58f7750c81692d6cee7
Cara Zellner <carajeanine@gmail.com>
2020-08-18 19:39:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,11 +27,12 @@ def position_taken?(board, index)
 end
   
 def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if vaild_move?(board, index)
-    move(board, index, current_player(board))turn(board)
-  end
-  display_board(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
+   display_board(board)
 end
