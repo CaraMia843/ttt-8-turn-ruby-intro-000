80940bc530323f34a2f577d19d364a823e76414a
Cara Zellner <carajeanine@gmail.com>
2020-08-18 19:33:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,10 +16,8 @@ def move(board, index, player = "X")
   board[index] = player
 end
 
-def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    return true
-  end
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
 end
   
 def turn(board)
